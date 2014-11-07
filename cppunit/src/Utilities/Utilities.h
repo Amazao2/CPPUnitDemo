82526e5a676ed0118917602a6fa94dd64d5b270f
NikLubz <nlubczynski@gmail.com>
2014-11-07 18:56:15 -0500

    Adding CppUnit Project

---
+++ cppunit/src/Utilities/Utilities.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+namespace Utilities
+{
+	inline const wchar_t* stringToWchar(const std::string& s)
+	{
+		return std::wstring(s.begin(), s.end()).c_str();
+	}
+
+	inline std::string cStringToString(const CString& s)
+	{
+		return std::string(CT2CA(s));
+	}
+}
