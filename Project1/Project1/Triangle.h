1a8c667f7eb6e9f648855964b2f6fab8d9ed3d2d
Brandon McRae <bmcrae2@uwo.ca>
2014-11-07 12:15:25 -0800

    - adds comments about requirements to Triangle.h and Square.h

--- Project1/Project1/Triangle.h
+++ Project1/Project1/Triangle.h
@@ -1,6 +1,17 @@
 #include "TriangleType.h"
 
+/*
+	Requirements for Triangles:
 
+		- triangles must have three sides
+		- any two side of a triangle must sum to a length greater than the third side to be
+		a valid triangle
+		- triangles are of three types:
+			- Scalene: all three sides of different lengths
+			- Isosceles: two sides are the same length, one is of different length
+			- Equilateral: all three sides are the same length
+		- triangles must be compared based on their perimeters
+*/
 class Triangle
 {
 private:
